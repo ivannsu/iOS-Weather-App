0fd3d97a252b51622cfc90651322922873d26b81
ivannsu <ivansu.webmail@gmail.com>
2019-03-08 21:34:35 +0700

    success made request to openweather api, and return a json result

--- iOS Weather App/Controllers/WeatherViewController.swift
+++ iOS Weather App/Controllers/WeatherViewController.swift
@@ -11,10 +11,11 @@ import CoreLocation
 import Alamofire
 import SwiftyJSON
 
-class WeatherViewController: UIViewController, CLLocationManagerDelegate {
+class WeatherViewController: UIViewController {
     
     @IBOutlet weak var conditionLabel: UILabel!
     
+    let WEATHER_API_KEY = "d46219087443a7abcb091c12140b29b2"
     let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
@@ -25,10 +26,37 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()
     }
     
+    func getWeatherData(lat: String, lon: String) {
+        let weatherParams: [String: String] = [
+            "lat": lat,
+            "lon": lon,
+            "appid": WEATHER_API_KEY
+        ]
+        
+        Alamofire.request(
+            "http://samples.openweathermap.org/data/2.5/weather",
+            method: .get,
+            parameters: weatherParams
+        ).responseJSON { response in
+            if response.result.isSuccess {
+                print("Success obtained weather api data")
+                print("\(response.result.value!)")
+            } else {
+                print("Error obtained Weather API Data: \(response.error!)")
+            }
+        }
+    }
+}
+
+extension WeatherViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations[locations.count - 1]
-        let latitude = location.coordinate.latitude
-        let longitude = location.coordinate.longitude
+        let latitude = String(location.coordinate.latitude)
+        let longitude = String(location.coordinate.longitude)
+        
+        getWeatherData(lat: latitude, lon: longitude)
+        
+        // print("\(latitude)")
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
@@ -36,9 +64,4 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
         
         conditionLabel.text = "Weather Unavailable"
     }
-    
-    func getWeatherData() {
-        
-    }
 }
-

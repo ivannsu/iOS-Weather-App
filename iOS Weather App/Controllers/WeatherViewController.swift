deabdfd6d3b07e4c1a3c482b8f934138861b5556
ivannsu <ivansu.webmail@gmail.com>
2019-03-09 13:56:45 +0700

    success give image icon fit the weather condition

--- iOS Weather App/Controllers/WeatherViewController.swift
+++ iOS Weather App/Controllers/WeatherViewController.swift
@@ -15,6 +15,7 @@ class WeatherViewController: UIViewController {
     
     @IBOutlet weak var conditionLabel: UILabel!
     @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var conditionIconImage: UIImageView!
     
     let WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather"
     let WEATHER_API_KEY = "d46219087443a7abcb091c12140b29b2"
@@ -65,9 +66,52 @@ class WeatherViewController: UIViewController {
     }
     
     func updateUI(data: JSON) {
-        // print("JSON Data: \(data)")
+        print("JSON Data: \(data)")
         conditionLabel.text = String(data["weather"][0]["main"].stringValue)
         cityNameLabel.text = String(data["name"].stringValue)
+        conditionIconImage.image = UIImage(named: getWeatherIcon(condition: data["weather"][0]["id"].intValue))
+    }
+    
+    func getWeatherIcon(condition: Int) -> String {
+        
+        switch (condition) {
+            
+        case 0...300 :
+            return "tstorm1"
+            
+        case 301...500 :
+            return "light_rain"
+            
+        case 501...600 :
+            return "shower3"
+            
+        case 601...700 :
+            return "snow4"
+            
+        case 701...771 :
+            return "fog"
+            
+        case 772...799 :
+            return "tstorm3"
+            
+        case 800 :
+            return "sunny"
+            
+        case 801...804 :
+            return "cloudy2"
+            
+        case 900...903, 905...1000  :
+            return "tstorm3"
+            
+        case 903 :
+            return "snow5"
+            
+        case 904 :
+            return "sunny"
+            
+        default :
+            return "dunno"
+        }
     }
 }
 
